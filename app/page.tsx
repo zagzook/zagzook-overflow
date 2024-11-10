8933d7a5cf4565c69b259f6e283fa1b2384dd00e
zagzook <ja0213@gmail.com>
2024-11-09 23:16:17 -0500

    tailwind CSS Setup abd Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black text-white">Welcome to the world of Next Js 15</h1>
+      <h1 className="h1-bold">Welcome to the world of Next Js 15</h1>
     </div>
   );
 }

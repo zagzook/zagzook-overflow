97a774de86b0c3dca03aaab596bdb5a17aaa3b4c
zagzook <ja0213@gmail.com>
2024-11-09 22:41:00 -0500

    ESLint and prettier setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import React from "react";
+
 import "./globals.css";
 
 const geistSans = localFont({
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Next Js 15</h1>
+      <h1 className="text-3xl font-black text-white">Welcome to the world of Next Js 15</h1>
     </div>
   );
 }
